d964eba8fc6d321e31ea26efe40ca84ad02b7899
Joss Dixon <josscdixon@gmail.com>
2022-03-14 16:08:11 +0000

    controller tests working

--- app/models/template_item.rb
+++ app/models/template_item.rb
@@ -1,6 +1,6 @@
 class TemplateItem < ApplicationRecord
   belongs_to :template
-  has_many :checklist_items
+  has_many :checklist_items, dependent: :destroy
 
   validates :action, presence: true
   validates :order, presence: true, numericality: true
