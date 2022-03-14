a9539823c77e22c4cb58f513f618328253081e3c
Joss Dixon <josscdixon@gmail.com>
2022-03-14 16:09:13 +0000

    updated template item model

--- app/models/template_item.rb
+++ app/models/template_item.rb
@@ -1,6 +1,6 @@
 class TemplateItem < ApplicationRecord
   belongs_to :template
-  has_many :checklist_items, dependent: :destroy
+  has_one :checklist_item, dependent: :destroy
 
   validates :action, presence: true
   validates :order, presence: true, numericality: true
